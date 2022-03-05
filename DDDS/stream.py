fd46abdff159ca9fd64d511e112c7b5b28345119
jan kral <jasiek.kral@gmail.com>
2022-03-05 16:18:34 +0100

    Running HRV graph

--- DDDS/stream.py
+++ DDDS/stream.py
@@ -6,13 +6,13 @@ import streamlit as st
 from contextlib import contextmanager
 from threading import current_thread
 from io import StringIO
-from collections import deque
 from PIL import Image
+from DDDS.combined_df import CombinedDFs
 from DDDS.annotations import Annotations
 from DDDS.hrv import HRV
 
 FRAMES_PER_SECOND = 30
-
+HRV_GRAPH_DURIATION = 100_000 #ms
 
 @contextmanager
 def st_redirect(src, dst):
@@ -21,14 +21,14 @@ def st_redirect(src, dst):
 
     with StringIO() as buffer:
         old_write = src.write
-
+        
         def new_write(b):
             if getattr(current_thread(), 'streamlit_script_run_ctx', None):
                 buffer.write(b)
                 output_func(buffer.getvalue())
             else:
                 old_write(b)
-
+        
         try:
             src.write = new_write
             yield
@@ -51,38 +51,58 @@ def st_stderr(dst):
 if 'frame_no' not in st.session_state:
     # First time loading, frame number set to zero
     st.session_state.frame_no = 0
+
 if 'instant' not in st.session_state:
     st.session_state.instant = 0
+
 if 'capture' not in st.session_state:
     # If video has not been loaded yet
     video_path = os.path.abspath('data/2021-11-22 15-40-50 eb0.flv')
     st.session_state.capture = cv.VideoCapture(video_path)
+
 if 'last_frame' not in st.session_state:
     # Load the first frame
     _, frame = st.session_state.capture.read()
     st.session_state.frame_no += 1
     st.session_state.last_frame = Image.fromarray(frame)
-if 'annotations' not in st.session_state:
-    with st_stdout('code'):
-        annotations = Annotations()
-    video_annotations = annotations.annotations[1].set_index('Instant')
-    seconds = pd.to_timedelta(video_annotations.index, unit='ms').seconds
-    minutes = np.floor(seconds / 60)
-    seconds = seconds - minutes*60
-    time = [f"{int(minute)}:{int(second)}" for minute, second in zip(minutes, seconds)]
-    video_annotations['time_lapsed'] = time
-    st.session_state.annotations = video_annotations
+
 if 'events' not in st.session_state:
     st.session_state.events = []
-if 'hrv' not in st.session_state:
-    hrv = HRV()
-    with st_stdout('code'):
-        hrv.get_dataframes()
-    data =  hrv.get_RR_series('22_11_2021_15_38 eb0')
-    st.session_state.hrv = pd.DataFrame(data[0], index=data[1])
-    
 
+if 'df' not in st.session_state:
+    with st_stdout('code'):
+        dfs = CombinedDFs()
+        st.session_state.df = dfs.combined_dfs[1]
+
+if 'annotations' not in st.session_state:    
+    # Select only annotations
+    df = st.session_state.df
+    annotations = df[- df['Instant'].isna()].reset_index().set_index('Instant')[['evenement']]
+    seconds = pd.to_timedelta(annotations.index, unit='ms').seconds
+    minutes = np.floor(seconds / 60)
+    time = [f"{int(minute)}:{int(second)}" for minute, second in zip(minutes, seconds)]
+    annotations['time_lapsed'] = time
+    st.session_state.annotations = annotations
     
+if 'hrv' not in st.session_state:
+    data = dfs.hrv.get_RR_series('22_11_2021_15_38 eb0')
+    data = pd.DataFrame({'RR':data[0], 'CumSum':data[1]})
+
+    # Timestamp Google for instant=0
+    ts_google = df[- df['Instant'].isna()].index[0]
+    instant = df[- df['Instant'].isna()].iloc[0]['Instant']
+    ts_google_zero = ts_google - pd.Timedelta(instant, unit='ms')
+
+    # Calculating offset
+    df = st.session_state.df
+    ts_google_first = df[df['RR_rate'] != '[]'].index[0]
+    offset = ts_google_zero - ts_google_first
+    instant_offset = round(offset.total_seconds()*1_000)
+
+    data['CumSum'] = data['CumSum'] - instant_offset
+    st.session_state.hrv = data.set_index('CumSum').loc[0:]
+
+
 frame_no = st.session_state.frame_no
 instant = st.session_state.instant
 capture = st.session_state.capture
@@ -113,21 +133,32 @@ with columns[0]:
     video = st.image(last_frame)
 
 ### GRAPH CONTAINER ###
-graph_container = st.container()
+graph_container = st.empty()
 with graph_container:
-    st.line_chart(st.session_state.hrv[0], use_container_width=True)
+    begin = instant - HRV_GRAPH_DURIATION
+    if begin < 0:
+        begin = 0
+    end = instant
+    st.line_chart(st.session_state.hrv['RR'].loc[begin:end])
 
 while play:
     play, frame = capture.read()
     frame_image = Image.fromarray(frame)
     st.session_state.frame_no += 1
+    st.session_state.instant = st.session_state.frame_no * 1_000/FRAMES_PER_SECOND # 30fps so 1 frame is 1/30sec
+    
     if st.session_state.frame_no % skip_frames == 0:
         video.image(frame_image)
+
+        # GRAPH CONTAINER
+        begin = st.session_state.instant - HRV_GRAPH_DURIATION
+        if begin < 0:
+            begin = 0
+        end = st.session_state.instant
+        graph_container.line_chart(st.session_state.hrv['RR'].loc[begin:end])
     
     
     frame_no_text.text(f"Frame number {st.session_state.frame_no}")
-
-    st.session_state.instant = st.session_state.frame_no * 1_000/FRAMES_PER_SECOND # 30fps so 1 frame is 1/30sec
     instant_text.text(f"Instant: {round(st.session_state.instant)}")
 
     annotations_to_display = annotations.loc[:st.session_state.instant]
@@ -138,5 +169,6 @@ while play:
             with event_container:
                 st.code(f"{event[0]} | {event[1]}")
             
+
 
     st.session_state.last_frame = frame_image
